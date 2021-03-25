0b902a1862d6e0063de62120b86fa13b3e07c769
HaythamB <49787517+HaythamB@users.noreply.github.com>
2021-03-25 13:53:40 +0100

    Update DuetWebAPI.py

--- DuetWebAPI.py
+++ DuetWebAPI.py
@@ -28,7 +28,7 @@ class DuetWebAPI:
     def __init__(self,base_url):
         self._base_url = base_url
         try:
-            print('Connecting to ', base_url, '..')
+            print('Connecting to', base_url, '..')
             URL=(f'{self._base_url}'+'/rr_status?type=2')
             r = self.requests.get(URL,timeout=(2,60))
             replyURL = (f'{self._base_url}'+'/rr_reply')
